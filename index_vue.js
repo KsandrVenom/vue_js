a5aeae10f7e7dde615524e7e86a21681f0b34f6b
KsandrVenom <clif007@yandex.ru>
2020-12-15 23:03:17 +0300

    подключил, директивы, списки, условия, кнопочки

---
+++ index_vue.js
@@ -0,0 +1,43 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    message: 'Привет, Vue'
+  }
+})
+
+var app2 = new Vue ({
+  el: '#app-2',
+  data: {
+    message: 'Вы загрузили эту страниу: ' + new Date() .toLocaleString()
+  }
+});
+
+var app3 = new Vue ({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue ({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Изучить JavaSript' },
+      { text: 'Изучить Vue' },
+      { text: 'Создать что-нибудь классное' }
+    ]
+  }
+});
+
+var app5 = new Vue ({
+  el: '#app-5',
+  data: {
+    message: 'Привет, Vue.js!',
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse('').join('')
+    }
+  }
+});

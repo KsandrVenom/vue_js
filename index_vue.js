431cf550608a9e476bbb73f58818b51572c025e0
KsandrVenom <clif007@yandex.ru>
2020-12-17 21:21:13 +0300

    создал первый компонент и добавил строчку ввода

--- index_vue.js
+++ index_vue.js
@@ -41,3 +41,28 @@ var app5 = new Vue ({
     }
   }
 });
+
+var app6 = new Vue ({
+  el: '#app-6',
+  data: {
+    message: 'Привет, Vue!'
+  }
+});
+
+Vue.component('todo-item', {
+
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue ({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      {id: 0, text: 'Овощи'},
+      {id: 1, text: 'Сыр'},
+      {id:2, text: 'Что там ещё люди едеят?'}
+    ]
+  }
+
+});
